9992c2271628b6886232e4fd0a86589a4d766a3a
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2024-09-11 18:03:45 -0400

    first echo in php

---
+++ echotext.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Première instruction : echo</title>
+</head>
+<body>
+  <h2>Affichage de texte avec PHP</h2>
+
+  <p>
+    Cette ligne a été écrite entièrement en HTML.<br/>
+    <?php echo "Celle-ci a été écrite en PHP"; ?>
+    <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?></p>
+  </p>
+</body>
+</html>
